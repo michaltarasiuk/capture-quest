5e3461b14c6abf7cd79427ad35519c9dae8b0a92
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-10-09 20:12:07 +0200

    feat: add spacer component for improved layout in RootPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import {CapturePhoto} from "@/components/CapturePhoto";
 import {FruitList} from "@/components/FruitList";
+import {Spacer} from "@heroui/react";
 
 export default function RootPage() {
   return (
     <>
       <FruitList />
+      <Spacer y={12} />
       <CapturePhoto />
     </>
   );
