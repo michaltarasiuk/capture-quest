941d431df52abcf1a012d8bcd6a5a19abdef1a2b
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-10-17 11:21:48 +0200

    add useIsMobile hook

--- src/components/QuestContent.tsx
+++ src/components/QuestContent.tsx
@@ -5,9 +5,11 @@ import {useRef} from "react";
 import {QuestDetails, SkeletonQuestDetails} from "@/components/QuestDetails";
 import {QuestList, SkeletonQuestList} from "@/components/QuestList";
 import {SkeletonStats, Stats} from "@/components/Stats";
+import {useIsMobile} from "@/hooks/use-is-mobile";
 import {cn} from "@/lib/cn";
 
 export function QuestContent() {
+  const isMobile = useIsMobile();
   const questDetailsRef = useRef<React.ComponentRef<typeof QuestDetails>>(null);
   function scrollToDetails() {
     questDetailsRef.current?.scrollIntoView({
@@ -19,7 +21,7 @@ export function QuestContent() {
     <>
       <Stats />
       <div className={cn("grid grid-cols-1 gap-4 lg:grid-cols-3")}>
-        <QuestList onNavigate={scrollToDetails} />
+        <QuestList onNavigate={() => isMobile && scrollToDetails()} />
         <QuestDetails ref={questDetailsRef} />
       </div>
     </>
