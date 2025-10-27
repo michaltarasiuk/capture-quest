d73df963f1029db4befa6d2e7ce9e2790b52a2f7
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-10-27 18:13:52 +0100

    simplify navigation handling in QuestsView component

--- src/components/QuestsView.tsx
+++ src/components/QuestsView.tsx
@@ -13,17 +13,16 @@ export function QuestsView() {
   const [detailsRef, isDetailsVisible, detailsEntry] = useIntersectionObserver({
     threshold: 0.5,
   });
+  function handleNavigation() {
+    if (isMobile() && !isDetailsVisible) {
+      detailsEntry!.target.scrollIntoView(AppScrollOptions);
+    }
+  }
   return (
     <>
       <Stats />
       <div className={cn("grid grid-cols-1 gap-4 lg:grid-cols-3")}>
-        <QuestList
-          onNavigate={() => {
-            if (isMobile() && !isDetailsVisible) {
-              detailsEntry?.target.scrollIntoView(AppScrollOptions);
-            }
-          }}
-        />
+        <QuestList onNavigate={handleNavigation} />
         <QuestDetails ref={detailsRef} />
       </div>
     </>
