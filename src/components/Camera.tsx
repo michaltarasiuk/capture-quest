07d749ec02748d04e46d42d84ac5fbf246bb3c72
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-30 20:26:45 +0100

    refactor: simplify CapturePhoto componen

---
+++ src/components/Camera.tsx
@@ -0,0 +1,81 @@
+import {Button, Skeleton} from "@heroui/react";
+import {CircleIcon, XIcon} from "lucide-react";
+import {useEffect, useRef, useState} from "react";
+import {useScrollLock} from "usehooks-ts";
+
+import {useEscapeDown} from "@/hooks/use-escape-down";
+import {cn} from "@/lib/cn";
+import {isDefined} from "@/lib/is-defined";
+
+interface CameraProps {
+  stream: MediaStream;
+  onCapture: (imageDataUrl: string) => void;
+  onClose: () => void;
+}
+
+export function Camera({stream, onCapture, onClose}: CameraProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    if (isDefined(videoRef.current)) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+  useScrollLock();
+  useEscapeDown(onClose);
+  async function capturePhoto() {
+    if (isDefined(videoRef.current) && isDefined(canvasRef.current)) {
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const context = canvas.getContext("2d");
+      if (isDefined(context)) {
+        context.drawImage(video, 0, 0);
+        const imageDataUrl = canvas.toDataURL("image/jpeg");
+        onCapture(imageDataUrl);
+      }
+    }
+  }
+  return (
+    <div className={cn("fixed inset-0 z-50 touch-none")}>
+      <Video ref={videoRef} />
+      <canvas ref={canvasRef} className={cn("hidden")} />
+      <Button
+        variant="flat"
+        radius="full"
+        className={cn("absolute end-4 top-4")}
+        isIconOnly
+        onPress={onClose}>
+        <XIcon className={cn("size-6")} />
+      </Button>
+      <Button
+        variant="flat"
+        size="lg"
+        radius="full"
+        className={cn("absolute start-1/2 bottom-10 -translate-x-1/2")}
+        isIconOnly
+        onPress={capturePhoto}>
+        <CircleIcon className={cn("size-10")} />
+      </Button>
+    </div>
+  );
+}
+
+function Video({ref}: {ref: React.Ref<HTMLVideoElement>}) {
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <>
+      {!isLoaded && <Skeleton className={cn("size-full")} />}
+      <video
+        ref={ref}
+        className={cn("size-full object-cover", {
+          hidden: !isLoaded,
+        })}
+        autoPlay
+        playsInline
+        onLoadedData={() => setIsLoaded(true)}
+      />
+    </>
+  );
+}
--- src/components/CapturePhoto.tsx
+++ src/components/CapturePhoto.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import {Button, Skeleton} from "@heroui/react";
-import {CameraIcon, CircleIcon, XIcon} from "lucide-react";
-import {useEffect, useRef, useState, useTransition} from "react";
+import {Button} from "@heroui/react";
+import {CameraIcon} from "lucide-react";
+import {useTransition} from "react";
 import {FocusScope} from "react-aria";
 import {createPortal} from "react-dom";
-import {useScrollLock} from "usehooks-ts";
 
-import {useEscapeDown} from "@/hooks/use-escape-down";
 import {useVideoStream} from "@/hooks/use-video-stream";
-import {cn} from "@/lib/cn";
 import {isDefined} from "@/lib/is-defined";
+
+import {Camera} from "./Camera";
 
 interface CapturePhotoProps {
   isDisabled: boolean;
@@ -45,79 +44,6 @@ export function CapturePhoto({isDisabled, onCapture}: CapturePhotoProps) {
           </FocusScope>,
           document.body,
         )}
-    </>
-  );
-}
-
-interface CameraProps {
-  stream: MediaStream;
-  onCapture: (imageDataUrl: string) => void;
-  onClose: () => void;
-}
-
-function Camera({stream, onCapture, onClose}: CameraProps) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    if (isDefined(videoRef.current)) {
-      videoRef.current.srcObject = stream;
-    }
-  }, [stream]);
-  useScrollLock();
-  useEscapeDown(onClose);
-  async function capturePhoto() {
-    if (isDefined(videoRef.current) && isDefined(canvasRef.current)) {
-      const video = videoRef.current;
-      const canvas = canvasRef.current;
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      const context = canvas.getContext("2d");
-      if (isDefined(context)) {
-        context.drawImage(video, 0, 0);
-        const imageDataUrl = canvas.toDataURL("image/jpeg");
-        onCapture(imageDataUrl);
-      }
-    }
-  }
-  return (
-    <div className={cn("fixed inset-0 z-50 touch-none")}>
-      <Video ref={videoRef} />
-      <canvas ref={canvasRef} className={cn("hidden")} />
-      <Button
-        variant="flat"
-        radius="full"
-        className={cn("absolute end-4 top-4")}
-        isIconOnly
-        onPress={onClose}>
-        <XIcon className={cn("size-6")} />
-      </Button>
-      <Button
-        variant="flat"
-        size="lg"
-        radius="full"
-        className={cn("absolute start-1/2 bottom-10 -translate-x-1/2")}
-        isIconOnly
-        onPress={capturePhoto}>
-        <CircleIcon className={cn("size-10")} />
-      </Button>
-    </div>
-  );
-}
-
-function Video({ref}: {ref: React.Ref<HTMLVideoElement>}) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  return (
-    <>
-      {!isLoaded && <Skeleton className={cn("size-full")} />}
-      <video
-        ref={ref}
-        className={cn("size-full object-cover", {
-          hidden: !isLoaded,
-        })}
-        autoPlay
-        playsInline
-        onLoadedData={() => setIsLoaded(true)}
-      />
     </>
   );
 }
