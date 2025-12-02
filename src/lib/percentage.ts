e1c6b848084e1a8aa92c7b59f3334c64c27c711e
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-12-02 22:04:18 +0100

    code refactor

--- src/lib/percentage.ts
+++ src/lib/percentage.ts
@@ -1,5 +1,11 @@
-export function percentage(v: number, base: number, decimals = 0) {
-  return !Number.isFinite(v) || !Number.isFinite(base) || base === 0
-    ? "0%"
-    : `${+((v / base) * 100).toFixed(decimals)}%`;
+export function percentage(v: number, total: number, decimals = 0) {
+  const percent =
+    !Number.isFinite(v) || !Number.isFinite(total) || total === 0
+      ? 0
+      : Number(((v / total) * 100).toFixed(decimals));
+  return formatPercentage(percent);
+}
+
+function formatPercentage(v: number) {
+  return v + "%";
 }
